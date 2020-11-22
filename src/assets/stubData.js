eecda77bd7cd9a031fda2d1d737be8b9a3bf2580
Tobias Baum <tbaum@tntinteractive.de>
2020-11-22 12:21:08 +0100

    Bisherigen Code grob in Angular-Struktur verschoben

---
+++ src/assets/stubData.js
@@ -0,0 +1,16 @@
+// Testdaten. Im Produktivbetrieb sollten die Daten stattdessen aus dem PHP-Skript der MRN-Webseite bezogen werden
+window.mrnData = {}
+
+window.mrnData.cards = [
+    {name: 'Respite'},
+    {name: 'Fog'},
+    {name: 'Forest'}
+]
+
+window.mrnData.decks = [
+    {
+        name: 'Tobis Deck',
+        cards: ['Fog', 'Fog', 'Respite', 'Forest', 'Forest', 'Forest']
+    }
+]
+
