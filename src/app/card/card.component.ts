9a0512bc0ccdb3ec7de343f158b3c0b4fed4f4a2
Tobias Baum <tbaum@tntinteractive.de>
2020-11-24 19:09:49 +0100

    Vergrößern von Karten

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -15,6 +15,8 @@ export class CardComponent implements OnInit {
   @Input()
   public act: string = '';
 
+  public large: boolean = false;
+
   constructor(private field: GameFieldStoreService) { }
 
   ngOnInit(): void {
