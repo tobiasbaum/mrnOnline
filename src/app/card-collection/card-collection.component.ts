d814f874b12ddc923f7eff65bc3fd7d23fdb0f83
Tobias Baum <tbaum@tntinteractive.de>
2020-11-25 19:56:28 +0100

    Kurz-Anzeige für CardCollections

--- src/app/card-collection/card-collection.component.ts
+++ src/app/card-collection/card-collection.component.ts
@@ -17,9 +17,38 @@ export class CardCollectionComponent implements OnInit {
   @Input()
   public act: string = '';
 
+  @Input()
+  public collapsed: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cardString(): string {
+    let counts: Map<string, number> = new Map();
+    this.collection.cards.forEach(c => {
+      if (counts.has(c.name)) {
+        counts.set(c.name, counts.get(c.name) as number + 1);
+      } else {
+        counts.set(c.name, 1);
+      }
+    });
+    let names: string[] = [];
+    this.collection.cards.forEach(c => {
+      if (names.indexOf(c.name) < 0) {
+        names.push(c.name);
+      }
+    });
+    return names
+      .map(n => {
+        if (counts.get(n) as number > 1) {
+          return n + ' (x' + counts.get(n) + ')';
+        } else {
+          return n;
+        }
+      })
+      .join(", ");
   }
 
 }
